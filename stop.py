678e99c96ae121224f3f0a46205390977f29eb78
NotLafuan <naufalbakri122@gmail.com>
2024-06-10 14:22:20 +0800

    Add direction data

--- stop.py
+++ stop.py
@@ -4,8 +4,9 @@ ser = serial.Serial('/dev/ttyACM0', 56700)
 
 angle = 90
 speed = 0
+direction = 0
 print('start')
-data = b'\x00'*5+b'A'+angle.to_bytes(1)+speed.to_bytes(1)+b'B'
+data = b'\x00'*5+b'A'+angle.to_bytes(1, 'big')+speed.to_bytes(1, 'big')+direction.to_bytes(1, 'big')+b'B'
 ser.write(data)
 print('finished')
 
