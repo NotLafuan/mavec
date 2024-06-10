2644c6b6f69e26c2d292de69e165dbeb21d9d338
NotLafuan <naufalbakri122@gmail.com>
2024-06-10 14:22:29 +0800

    Add move control

---
+++ move.py
@@ -0,0 +1,25 @@
+import serial
+import time
+
+
+ser = serial.Serial('/dev/ttyACM0', 56700)
+
+
+def move(angle: int, speed: int, dir: int):
+    data = b'\x00'*5+b'A' + \
+        angle.to_bytes(1, 'big')+speed.to_bytes(1, 'big') + \
+        dir.to_bytes(1, 'big')+b'B'
+    ser.write(data)
+
+if __name__ == '__main__':
+    move(90, 30, 0)
+    time.sleep(1)
+    move(70, 30, 1)
+    time.sleep(1)
+    move(90, 30, 1)
+    time.sleep(1)
+    move(110, 30, 1)
+    time.sleep(1)
+    move(90, 0, 1)
+
+ser.close()
