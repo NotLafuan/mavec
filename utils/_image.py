73390293f0e08061f21399d2b1d830a58ca52c2d
NotLafuan <naufalbakri122@gmail.com>
2024-07-03 18:40:08 +0800

    Remove points correction for warping

--- utils/_image.py
+++ utils/_image.py
@@ -11,17 +11,21 @@ def order_points(pts):
     # the top-left point will have the smallest sum, whereas
     # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    #rect[0] = pts[np.argmin(s)]
+    #rect[2] = pts[np.argmax(s)]
 
     # now, compute the difference between the points, the
     # top-right point will have the smallest difference,
     # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis=1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
+    #rect[1] = pts[np.argmin(diff)]
+    #rect[3] = pts[np.argmax(diff)]
 
     # return the ordered coordinates
+    rect[0] = pts[0]
+    rect[1] = pts[1]
+    rect[2] = pts[2]
+    rect[3] = pts[3]
     return rect
 
 
